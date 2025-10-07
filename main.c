7cf5f58df2b9ab41744935cea9bc5e0eb7e71eda
Amit Judes <amit@judes.co.il>
2025-10-07 12:12:33 +0300

    started writing the printMenu func

--- main.c
+++ main.c
@@ -1,26 +1,50 @@
 #include "fileManager/fileManager.h"
 
+void printMenu(taskList* tasks, taskList* done);
+
 int main() {
-    fileManager fm = {0, 0, 0, 0};
-    taskList list = empty_list();
+    taskList tasks = empty_list();
+    taskList done = empty_list();
+    printMenu(&tasks, &done);
+}
 
-    // open file
-    fm.fp = fopen("..\\TODOs.txt", "r");
-    if (NULL == fm.fp) {
-        printf("couldn't open file\n");
-        exit(EXIT_FAILURE);
+void printMenu(taskList* tasks, taskList* done) {
+    printf("Welcome to your todo list manager\n");
+    size_t choice = 0;
+    char taskName[50];
+    while (choice != 6) {
+        printf("Please choose one of the following options to continue\n");
+        printf("1. Print tasks\n");
+        printf("2. Add task\n");
+        printf("3. Mark task as done\n");
+        printf("4. Remove task\n");
+        printf("5. Print done tasks\n");
+        printf("6. Exit\n");
+
+        scanf(" %d", &choice);
+        while (getchar() != '\n'); // clear leftover '\n'
+
+        switch (choice) {
+        case 1:
+            print_list(tasks);
+            break;
+        case 2:
+            printf("Name the task: ");
+
+            scanf(" %s", &taskName);
+            while (getchar() != '\n'); // clear leftover '\n'
+
+            printf("The task %s has been added\n");
+
+            push_back(tasks, taskName);
+            break;
+        case 6:
+            break;
+        default:
+            printf("Invalid input, please try again\n");
+            break;
+        }
+        printf("\n");
     }
-    
-    // move line by line
-    while (fm.read = getline(&fm.line, &fm.len, fm.fp) != -1) {
-        printf("Retrieved line of lend %i: \n", fm.read);
-        printf("%s", fm.line);
-    }
-
-    // close file
-    fclose(fm.fp);
-    if (fm.line)
-        free(fm.line);
-
-    return 0;
+    printf("Goodbye");
 }
